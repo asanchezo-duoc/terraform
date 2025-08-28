79cf692e05de884a9992c1574d410484e87f9cdb
asanchezo-duoc <a.sanchezo@profesor.duoc.cl>
2025-08-27 20:04:58 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,8 +16,7 @@ provider "azurerm" {
   subscription_id = "bb1ccac7-d7f8-47bf-82c2-f223185cfab9"
 }
 
-# Grupo de recursos
-resource "azurerm_resource_group" "rg" {
-  name     = "AreaInfraestructura"
-  location = "East US 2"
+# Referencia al grupo de recursos existente
+data "azurerm_resource_group" "rg" {
+  name = "AreaInfraestructura"
 }
