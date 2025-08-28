c4bf8db2775ff891d4f1b5f4a84a9d15ab1d42e3
asanchezo-duoc <a.sanchezo@profesor.duoc.cl>
2025-08-27 20:03:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,6 @@ provider "azurerm" {
 
 # Grupo de recursos
 resource "azurerm_resource_group" "rg" {
-  name     = "infracomocodigoduoc2025"
+  name     = "AreaInfraestructura"
   location = "East US 2"
 }
