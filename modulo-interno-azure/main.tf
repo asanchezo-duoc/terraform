fe657e92b2429b80d3d4e4eca8d62aa8a9f3e89e
asanchezo-duoc <a.sanchezo@profesor.duoc.cl>
2025-08-27 20:29:45 -0400

    Update main.tf

--- modulo-interno-azure/main.tf
+++ modulo-interno-azure/main.tf
@@ -11,3 +11,44 @@ resource "azurerm_subnet" "my_vms_net" {
   virtual_network_name = azurerm_virtual_network.my_vnet.name
   address_prefixes     = ["10.200.0.0/23"]
 }
+
+# Interfaz de red
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.myself}-nic"
+  location            = var.region
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.my_vms_net.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Create a Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = "${var.myself}-vm1-linux"
+  resource_group_name             = var.resource_group_name
+  location                        = var.region
+  size                            = "Standard_DS1_v2"
+  admin_username                  = "azureuser"
+  disable_password_authentication = true
+  network_interface_ids           = [azurerm_network_interface.nic.id]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
